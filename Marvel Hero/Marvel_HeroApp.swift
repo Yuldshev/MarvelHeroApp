172c0733cbfc4b8ae92046beb3df5ac8032cca66
Yuldshev <ikrom921@gmail.com>
2025-06-24 10:49:31 +0500

    Initial Commit

---
+++ Marvel Hero/Marvel_HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_HeroApp.swift
+//  Marvel Hero
+//
+//  Created by T on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
